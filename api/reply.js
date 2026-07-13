83eb05964a4823dd52de3a687086c939f50004e3
DRRCMW <133627484+DRRCMW@users.noreply.github.com>
2026-07-12 23:24:13 -0700

    Update reply.js

--- api/reply.js
+++ api/reply.js
@@ -60,7 +60,7 @@ export default async function handler(req, res) {
   // Instant TwiML auto-reply (Twilio delivers it â no extra send needed)
   res.setHeader('Content-Type', 'text/xml');
   if (isStop) return res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
-  const msg = `Thanks for getting back to us! This is Robyn with Akira Digital. Grab any 15-min slot that works for you: cal.com/akira-digital or just reply with a good time to call.`;
+  const msg = `Thanks for replying! This is Mercedes with Akira Digital. Robyn would love to connect — grab a free 15-min slot here: cal.com/akira-digital or text back a good time and she'll call you directly.`;
   return res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?><Response><Message>${msg}</Message></Response>`);
 }
 
